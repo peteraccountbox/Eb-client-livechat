572d89fbe03021e8ad3646e8e2f23ff8cafb8f40
Ibrahim7987 <ibrahim@engagebay.co>
2024-09-03 17:26:16 +0530

    customer identifn

--- src/components/interactiveNodes/CustomerIdentification.tsx
+++ src/components/interactiveNodes/CustomerIdentification.tsx
@@ -50,49 +50,16 @@ const CustomerIdentification: React.FC<InteractiveNodeProps> = ({
       is_valid: true,
       display: false,
     },
-
-    // {
-    //   name: "date",
-    //   label: "When did the issue occur",
-    //   type: "date",
-    //   required: true,
-    //   value: "",
-    //   placeholder: "",
-    //   error: "",
-    //   is_valid: true,
-    // },
   ];
   const [formFields, setFormFields] = useState<any[]>([
     fields[0],
     fields.find((field) => execution.node.data.formData.sign_in == field.name),
   ]);
-  // const [toggleField, setToggleField] = useState<any>(
-  //   fields.find((field) => execution.node.data.formData.sign_in == field.name)
-  // );
+
   const [saving, setSaving] = useState(false);
 
-  const [showSignIn, setShowSignIn] = useState(execution.executed);
+  const [showSignIn, setShowSignIn] = useState(true);
 
-  // const handleFieldValueChange = (value: string, field: any) => {
-  //   const results = formFields.map((eachField) => {
-  //     if (eachField.name == field.name) eachField.value = value;
-  //     if (eachField.name == "sign_in") eachField.value = field.name;
-  //     if (eachField.error) eachField = isValidTicketField(eachField);
-  //     return eachField;
-  //   });
-  //   setFormFields(results);
-  // };
-
-  // const onToggle = (value: string) => {
-  //   console.log("onToggle ", value);
-  //   if (value == "email") {
-  //     setToggleField(fields[1]);
-  //     setFormFields([{ ...formFields[0], value: value }, fields[1]]);
-  //   } else {
-  //     setToggleField(fields[2]);
-  //     setFormFields([{ ...formFields[0], value: value }, fields[2]]);
-  //   }
-  // };
   const submitForm = () => {
     if (isvalidForm() && !saving) {
       setSaving(true);
@@ -150,106 +117,24 @@ const CustomerIdentification: React.FC<InteractiveNodeProps> = ({
       ) : executionMeta &&
         !executionMeta.isCompleted &&
         !execution.nextNodeId ? (
-        // <div className="chat__ticket-form-main">
-        //   <div className="chat__ticket-form">
-        //     <div className="chat__ticket-form-group">
-        //       <label className="chat__ticket-form-label">
-        //         {fields[0].label} <span className="theme-color">*</span>
-        //       </label>
-        //       <div style={{ display: "flex" }}>
-        //         <div
-        //           className="chat__form-check radio-options"
-        //           style={{ width: "25%" }}
-        //         >
-        //           <input
-        //             type="radio"
-        //             id="email"
-        //             value="email"
-        //             required={fields[0].required}
-        //             name={fields[0].name}
-        //             checked={formFields[0].value == "email"}
-        //             onChange={(e: any) => onToggle(e.target.value)}
-        //             className="chat__form-check-input"
-        //           />
-        //           <label className="chat__form-check-label" htmlFor="email">
-        //             Email
-        //           </label>
-        //         </div>
-        //         <div className="chat__form-check radio-options">
-        //           <input
-        //             type="radio"
-        //             id="sms"
-        //             value="phone"
-        //             required={fields[0].required}
-        //             name={fields[0].name}
-        //             checked={formFields[0].value == "phone"}
-        //             onChange={(e: any) => onToggle(e.target.value)}
-        //             className="chat__form-check-input"
-        //           />
-        //           <label className="chat__form-check-label" htmlFor={"sms"}>
-        //             SMS
-        //           </label>
-        //         </div>
-        //       </div>
-
-        //       {fields[0].error ? (
-        //         <div className="error-content">{fields[0].error}</div>
-        //       ) : (
-        //         <></>
-        //       )}
-        //     </div>
-        //     <div className="chat__ticket-form-group">
-        //       <label className="chat__ticket-form-label">
-        //         {toggleField.label} <span className="theme-color">*</span>
-        //       </label>
-        //       <input
-        //         type={toggleField.type}
-        //         placeholder={toggleField.placeholder}
-        //         required={toggleField.required}
-        //         name={toggleField.name}
-        //         className="chat__ticket-form-control"
-        //         value={toggleField.value}
-        //         onChange={(e) =>
-        //           handleFieldValueChange(e.target.value, toggleField)
-        //         }
-        //       />
-        //       {toggleField.error ? (
-        //         <div className="error-content">{toggleField.error}</div>
-        //       ) : (
-        //         <></>
-        //       )}
-        //     </div>
-
-        //     <span
-        //       style={{
-        //         display: "flex",
-        //         alignItems: "center",
-        //         justifyContent: "center",
-        //       }}
-        //     >
-        //       <button
-        //         className="chat__messages-btn"
-        //         onClick={submitForm}
-        //         style={{
-        //           display: "flex",
-        //           alignItems: "center",
-        //           width: "auto",
-        //           cursor: "pointer",
-        //           marginTop: "20px",
-        //         }}
-        //       >
-        //         {saving ? "Saving ..." : "Sign In"}
-        //       </button>
-        //     </span>
-        //   </div>
-        // </div>
-        <LoginForm
-          sign_in={execution.node.data.formData.sign_in}
-          submitForm={submitForm}
-          formFields={formFields}
-          setFormFields={setFormFields}
-          saving={saving}
-        />
+        <>
+          <div className="chat__messages-group">
+            <ul className="chat__messages-list">
+              <div className="chat__messages-list-item">
+                <div className="chat__messages-bubble chat__message-type-TEXT">
+                  <span className="actual">Sign in to continue</span>
+                </div>
+              </div>
+            </ul>
+          </div>
+          <LoginForm
+            sign_in={execution.node.data.formData.sign_in}
+            submitForm={submitForm}
+            formFields={formFields}
+            setFormFields={setFormFields}
+            saving={saving}
+          />
+        </>
       ) : (
         <div className="chat__messages-group">
           <ul className="chat__messages-list">
