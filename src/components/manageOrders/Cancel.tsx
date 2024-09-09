c4199d1fb4b5e2b589ec64bc4bf49198b602c7fd
Ibrahim7987 <ibrahim@engagebay.co>
2024-09-09 18:15:27 +0530

    address

--- src/components/manageOrders/Cancel.tsx
+++ src/components/manageOrders/Cancel.tsx
@@ -34,7 +34,7 @@ const Cancel = (props: any) => {
           I'd like to cancel the following fulfillment:
           <br />
           ---------------------------------------
-          <OrderDetails order={order} />
+          {orderDetails && <OrderDetails order={order} />}
         </>
       ),
       bodyText: renderToString(
@@ -42,7 +42,7 @@ const Cancel = (props: any) => {
           I'd like to cancel the following fulfillment:
           <br />
           ---------------------------------------
-          <OrderDetails order={order} />
+          {orderDetails && <OrderDetails order={order} />}
         </>
       ),
       format: MessageFormatType.TEXT,
