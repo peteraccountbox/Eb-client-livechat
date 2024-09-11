1e0c80de10c35876721e7039e1322e9d56854028
vaishnavi-accountbox <vaishnavi@engagehub.io>
2024-09-11 15:16:43 +0530

    changes

--- src/components/manageOrders/Orders.tsx
+++ src/components/manageOrders/Orders.tsx
@@ -47,101 +47,109 @@ const Orders = () => {
   }, []);
   return (
     <>
-      {!fetching && orders.length === 0 ? (
-        <>
-          <div className="no__order_data_wrapper">
-            <div className="no__order_data_content">
-              <img
-                className="pad-no-content-img"
-                src="https://d2p078bqz5urf7.cloudfront.net/cloud/assets/livechat/no-chats-yet.svg"
-                alt="No Orders"
-              />
-              <h2 className="pad-content-title">No Orders</h2>
-              <p className="pad-text">There is no record of previous orders.</p>
-            </div>
-          </div>
-        </>
-      ) : (
-        <div className="orders__collections">
-          <header>
-            <h2 className="orders__collections-title">My orders</h2>
-          </header>
-          <div className="orders__collections-list">
-            {orders.length > 0 &&
-              orders &&
-              orders.map((order: any) => {
-                const orderDetails = JSON.parse(order.meta);
+      {
+        fetching ? <>Loading...</> : <>
 
-                if (
-                  !orderDetails.line_items ||
-                  orderDetails.line_items.length == 0
-                )
-                  return <></>;
 
-                return (
-                  <div
-                    className="orders__collections-list-item"
-                    onClick={() => action(order)}
-                  >
-                    <div
-                      className="orders__collections-list-order-header"
-                      onClick={() => action(order)}
-                    >
-                      <div className="orders__collections-list-order-header-title">
-                        Order {orderDetails.name}
-                      </div>
+          {!fetching && orders.length === 0 ? (
+            <>
+              <div className="no__order_data_wrapper">
+                <div className="no__order_data_content">
+                  <img
+                    className="pad-no-content-img"
+                    src="https://d2p078bqz5urf7.cloudfront.net/cloud/assets/livechat/no-chats-yet.svg"
+                    alt="No Orders"
+                  />
+                  <h2 className="pad-content-title">No Orders</h2>
+                  <p className="pad-text">There is no record of previous orders.</p>
+                </div>
+              </div>
+            </>
+          ) : (
+            <div className="orders__collections">
+              <header>
+                <h2 className="orders__collections-title">My orders</h2>
+              </header>
+              <div className="orders__collections-list">
+                {orders.length > 0 &&
+                  orders &&
+                  orders.map((order: any) => {
+                    const orderDetails = JSON.parse(order.meta);
 
-                      <div className="orders__collections-list-order-header-end">
-                        <span>
-                          {orderDetails.currency}
-                          {orderDetails.current_total_price}
-                        </span>
-                      </div>
-                    </div>
-                    <div className="orders__collections-list-fulfillment-header">
-                      <div className="orders__collections-list-fulfillment-header-title">
-                        Shipment
-                      </div>
-                      <div className="orders__collections-list-fulfillment-header-status">
-                        {orderDetails.fulfillment_status && (
-                          <span className="orders__collections-list-fulfillment-header-badge">
-                            {orderDetails.fulfillment_status}
-                          </span>
-                        )}
-                      </div>
-                    </div>
-                    <ManageActions />
+                    if (
+                      !orderDetails.line_items ||
+                      orderDetails.line_items.length == 0
+                    )
+                      return <></>;
 
-                    {orderDetails.line_items &&
-                      orderDetails.line_items.map((item: any) => (
-                        <div className="orders__collections-line-items-group">
-                          <div className="orders__collections-line-items">
-                            <div className="orders__collections-line-items-avatar">
-                              <img
-                                src={`${item.product_image_url
-                                  ? item.product_image_url
-                                  : "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                  }`}
-                              />
-                            </div>
-                            <div className="orders__collections-line-items-details">
-                              <div className="orders__collections-line-items-name">
-                                {item.name}
-                              </div>
-                              <div className="orders__collections-line-items-currency">
-                                {orderDetails.currency}
-                                {item.price} <span>x{item.quantity}</span>
-                              </div>
-                            </div>
+                    return (
+                      <div
+                        className="orders__collections-list-item"
+                        onClick={() => action(order)}
+                      >
+                        <div
+                          className="orders__collections-list-order-header"
+                          onClick={() => action(order)}
+                        >
+                          <div className="orders__collections-list-order-header-title">
+                            Order {orderDetails.name}
+                          </div>
+
+                          <div className="orders__collections-list-order-header-end">
+                            <span>
+                              {orderDetails.currency}
+                              {orderDetails.current_total_price}
+                            </span>
                           </div>
                         </div>
-                      ))}
-                  </div>
-                );
-              })}
-          </div>
-        </div>
-      )}
+                        <div className="orders__collections-list-fulfillment-header">
+                          <div className="orders__collections-list-fulfillment-header-title">
+                            Shipment
+                          </div>
+                          <div className="orders__collections-list-fulfillment-header-status">
+                            {orderDetails.fulfillment_status && (
+                              <span className="orders__collections-list-fulfillment-header-badge">
+                                {orderDetails.fulfillment_status}
+                              </span>
+                            )}
+                          </div>
+                        </div>
+                        <ManageActions />
+
+                        {orderDetails.line_items &&
+                          orderDetails.line_items.map((item: any) => (
+                            <div className="orders__collections-line-items-group">
+                              <div className="orders__collections-line-items">
+                                <div className="orders__collections-line-items-avatar">
+                                  <img
+                                    src={`${item.product_image_url
+                                      ? item.product_image_url
+                                      : "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                                      }`}
+                                  />
+                                </div>
+                                <div className="orders__collections-line-items-details">
+                                  <div className="orders__collections-line-items-name">
+                                    {item.name}
+                                  </div>
+                                  <div className="orders__collections-line-items-currency">
+                                    {orderDetails.currency}
+                                    {item.price} <span>x{item.quantity}</span>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          ))}
+                      </div>
+                    );
+                  })}
+              </div>
+            </div>
+          )}
+
+        </>
+      }
+
     </>
   );
 };
