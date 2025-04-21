5f1e8137ec7c9ccf974b23c60fd88d39b6cdafb3
Ibrahim7987 <ibrahim@engagebay.co>
2025-04-21 16:05:13 +0530

    builde

--- build.sh
+++ build.sh
@@ -4,11 +4,20 @@ npm run build
 
 
 if [ "$1" ]; then
+  # Determine the base path based on $1
+  if [ "$1" = "app" ]; then
+    BASE_PATH="eb-file-repo/onsite/js"
+  else
+    BASE_PATH="eb-file-repo/onsite/js/$1"
+  fi
 
+  npx wrangler r2 object put $BASE_PATH/chat/main/main.min.js \ 
+    --file=/home/eb137/Documents/Eb-client-livechat/build/main/main.min.js
+    --remote
 
-npx wrangler r2 object put reacho-file-repo/onsite/js/chat/main/main.min.js --file=/home/eb137/Documents/Eb-client-livechat/build/main/main.min.js
-
-npx wrangler r2 object put reacho-file-repo/onsite/js/chat/loader/main.min.js --file=/home/eb137/Documents/Eb-client-livechat/build/loader/main.min.js
+  npx wrangler r2 object put $BASE_PATH/chat/loader/main.min.js \
+    --file=/home/eb137/Documents/Eb-client-livechat/build/loader/main.min.js
+    --remote
 
 else
   echo "No CDN2"  
