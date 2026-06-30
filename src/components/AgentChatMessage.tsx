a988f67e7952cdfe3b9138dd6ab7d85b654bdb1d
Ibrahim7987 <ibrahim@engagebay.co>
2026-06-30 13:10:32 +0530

    css changes

--- src/components/AgentChatMessage.tsx
+++ src/components/AgentChatMessage.tsx
@@ -72,7 +72,7 @@ const AgentChatMessage: FC<AgentChatMessagePropsType> = (props) => {
               </li>
             </ul>
           </div>
-          {props.message.lastAction == "EDITED" && <span style={{paddingLeft: "45px", fontSize: "14px", color: "gray"}}>Edited</span>}
+          {props.message.lastAction == "EDITED" && <span style={{paddingLeft: "45px", fontSize: "12px", color: "gray"}}>Edited</span>}
 
           {/* <div className="chat__messages-timestamp"> */}
           {/* <>{getMessageTime()}</> */}
